e22d27400ddea8049017253931b6e28e3c5bf703
Nishit956 <nishitdalal25@gmail.com>
2025-06-02 19:54:35 +0530

    Added socket.io

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,7 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 import cloudinary from "../lib/cloudinary.js";
+import { io,userSocketMap } from "../server.js";
 
 // Get all the user except logged in user
 export const getUsersForSidebar = async (req,res) => {
@@ -75,6 +76,12 @@ export const sendMessage = async (req,res) =>{
             text,
             image: imageUrl
         })
+
+        // Emit the new message to the receiver's socket
+        const receiverSocketId = userSocketMap[receiverId];
+        if (receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
 
         res.json({success:true, newMessage});
          
